f8bde67f6955e4cbcf149b8e57a94770a42f6ef1
ddimitrov777 <ddimitro@chicagobooth.edu>
2021-04-12 23:50:06 +0000

    asdfasdf

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -22,18 +22,35 @@ class Matrix
     split.each do |item|
       newarray += item.split(/ /)
     end
-    
+
     newarray.map(&:to_i)
     #newarray.grep(/\d+/, &:to_i)
     #newarray.map { |n| n.to_i }
     #arraysplit = split.each { |n| n.split(/ /)}
     finalarray = newarray.each_slice(2).to_a
-    finalarray.map { |sr| sr.map(&:to_i) }
+
+    @seriouslyfinalarray = finalarray.map { |sr| sr.map(&:to_i) }
+    return @seriouslyfinalarray
+
+    @numcols = @seriouslyfinalarray[0].count - 1
   end
 
   def columns
+    therange = Range.new(0, @numcols)
+    width = (@numcols + 1)
+    height = (@seriouslyfinalarray.count - 1)
+    colarray = Array.new(height + 1) { Array.new(width) }
+    range2 = Range.new(0, height)
+
+    therange.each do |x|
+      range2.each do |y|
+        colarray[x][y] = @seriouslyfinalarray[y][x]
+      end
+    end
+    return colarray
   end
 end
 
 test = Matrix.new("1 2\n10 20")
-p test.rows[0]
+p test.columns
+#p test.rows[1]
