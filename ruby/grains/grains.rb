eb6cf1cb22770813bc065c4d0ea17f36965d833f
ddimitrov777 <ddimitro@chicagobooth.edu>
2021-04-13 23:56:22 +0000

    got my solution

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -6,3 +6,16 @@ To get started with TDD, see the `README.md` file in your
 `ruby/grains` directory.
 =end
 
+class Grains
+    def self.square(num)
+        @indexnum = num - 1
+        thearray = Array.new
+        (0..63).each do |n|
+          thearray << (2 ** n)
+        end
+        return thearray[@indexnum]
+    end
+end
+
+
+#p Grains.square(7)
