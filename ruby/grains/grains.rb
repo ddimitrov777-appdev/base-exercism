1bc2b264976598dfccc78af996c9d2511d6e1a60
ddimitrov777 <ddimitro@chicagobooth.edu>
2021-04-14 00:04:14 +0000

    asdfsdf

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -7,15 +7,25 @@ To get started with TDD, see the `README.md` file in your
 =end
 
 class Grains
-    def self.square(num)
+    def self.square(num = 64)
+        if num <= 0
+        raise ArgumentError
+        else
         @indexnum = num - 1
         thearray = Array.new
         (0..63).each do |n|
-          thearray << (2 ** n)
+          @thearray << (2 ** n)
         end
-        return thearray[@indexnum]
+        return @thearray[@indexnum]
+        end
+    end
+
+    def self.total
+        Grains.square(64)
+        thetotal = @thearray.inject(0){|sum,x| sum + x }
     end
 end
 
 
-#p Grains.square(7)
+p Grains.square(4)
+p Grains.total
